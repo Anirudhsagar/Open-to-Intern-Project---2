525993caa8ce3eacc5646a6c538efad8d8ef8cda
anirudh sagar <you@example.com>
2022-09-14 10:18:55 +0530

    update

---
+++ src/models/internModel.js
@@ -0,0 +1,35 @@
+
+
+
+const internSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required:true, 
+        trim:true,
+    },
+    email: {
+        type: String,
+        required:true ,
+        unique: true,
+        trim: true
+    },
+    mobile: {
+        type: String,
+        required:true ,
+        unique: true,
+        trim:true
+    },
+    collegeId: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: 'college',
+        trim:true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model("intern", internSchema)
