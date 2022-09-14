b488a0ed29cbce97924ea56a196ad43e42c16e94
vishalnpasi <vishalnpasi@gmail.com>
2022-09-14 11:10:26 +0530

    a

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+mongoose.connect("mongodb+srv://vishalpasi:FbiA1ChEDTbvv6eL@cluster0.3xmrakz.mongodb.net/group64database", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
