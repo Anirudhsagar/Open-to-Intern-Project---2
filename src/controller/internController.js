d904e039bb4729817443167142b6cb47d2dff25f
vishalnpasi <vishalnpasi@gmail.com>
2022-09-14 15:52:54 +0530

    final code

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,8 +1,12 @@
 const internModel = require('../models/internModel')
 const collageModel = require('../models/collegeModel')
-const Valid = require('../../validation/validation');
 const collegeModel = require('../models/collegeModel');
 
+const isValid = function(value) {
+    if(typeof (value) == "undefined" || typeof (value) == null) return false
+    if(typeof (value) == "string" && (value).trim().length == 0)return false
+    return true
+}   
 const regName = /^[A-Z a-z]+$/
 const regforNo = /^[0-9]{10}$/;
 const regforemail =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
@@ -13,20 +17,20 @@ const createIntern = async function (req, res) {
         
         const { name, mobile, email, collegeName } = req.body
 
-        if (!Valid.isValid(name)) return res.status(400).send({ status: false, message: "name is mendatory" })
+        if (!isValid(name)) return res.status(400).send({ status: false, message: "name is mendatory" })
         if(!regName.test(name)) return res.status(400).send({ status: false, message: "name is invilid" })
 
-        if (!Valid.isValid(email)) return res.status(400).send({ status: false, message: "email is mendatory" })
+        if (!isValid(email)) return res.status(400).send({ status: false, message: "email is mendatory" })
         if(!regforemail.test(email)) return res.status(400).send({ status: false, message: "email is invilid" })
         let foundEmail = await internModel.findOne({email:email})
         if(foundEmail) return res.status(400).send({ status: false, message: "Email is already exist" })
 
-        if (!Valid.isValid(mobile) ) return res.status(400).send({ status: false, message: "Mobile is mendatory" })
+        if (!isValid(mobile) ) return res.status(400).send({ status: false, message: "Mobile is mendatory" })
         if(!regforNo.test(mobile)) return res.status(400).send({ status: false, message: "Mobile is invilid" })
         let foundMobile = await internModel.findOne({mobile:mobile})
         if(foundMobile) return res.status(400).send({ status: false, message: "Mobile Number is already exist" })
 
-        if (!Valid.isValid(collegeName)) return res.status(400).send({ status: false, message: "collegeName is mendatory" })
+        if (!isValid(collegeName)) return res.status(400).send({ status: false, message: "collegeName is mendatory" })
 
         let collegeDetails = await collageModel.findOne({ name: collegeName })
         if (!collegeDetails) return res.status(400).send({ status: false, message: "college is not exist" })
--- validation/validation.js
+++
@@ -1,6 +0,0 @@
-const isValid = function(value) {
-    if(typeof (value) == "undefined" || typeof (value) == null) return false
-    if(typeof (value) == "string" && (value).trim().length == 0)return false
-    return true
-}   
-module.exports = {isValid}
